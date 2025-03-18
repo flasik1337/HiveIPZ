b0f4927b106f0dd2bb20438f0a642a5f63c84b30
ffojna <ffojna36@gmail.com>
2025-03-18 23:19:56 +0100

    Ikony w listach wyszukiwania; funkcja dobierająca ikone do wydarzenia na podstawie typu

---
+++ aplikacja/lib/styles/hive_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class HiveColors {
+  static const Color main = Color(0xFFFFC300);
+  static const Color free = Color(0xFF388E3C);
+  static const Color strongAccent = Color(0xFFFFC107);
+}
